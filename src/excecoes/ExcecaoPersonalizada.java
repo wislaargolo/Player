c820dc87b4a3419c81d598c4ce77eac7d7919a58
wislaargolo <wislaargolo1@gmail.com>
2023-12-14 16:02:11 -0300

    javadoc excecoes/

--- src/excecoes/ExcecaoPersonalizada.java
+++ src/excecoes/ExcecaoPersonalizada.java
@@ -1,12 +1,18 @@
 package excecoes;
 
+/**
+ * Exceção personalizada que estende RuntimeException para representar situações específicas
+ * da aplicação.
+ */
 public class ExcecaoPersonalizada extends RuntimeException {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+     * Construtor que cria uma nova ExcecaoPersonalizada com uma mensagem específica.
+     *
+     * @param message A mensagem de erro que descreve o que causou a exceção.               
+     */
 	public ExcecaoPersonalizada(String message) {
         super(message);
     }
