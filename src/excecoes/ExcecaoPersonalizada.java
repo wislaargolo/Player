81a334e567825ec4fbd62b80629f8074d06f43b3
“Rubens <rubensmatheusvenancio@gmail.com>
2023-12-14 16:04:07 -0300

    javaDoc do dao de diretorios

--- src/excecoes/ExcecaoPersonalizada.java
+++ src/excecoes/ExcecaoPersonalizada.java
@@ -2,9 +2,7 @@ package excecoes;
 
 public class ExcecaoPersonalizada extends RuntimeException {
 
-    /**
-	 * 
-	 */
+   
 	private static final long serialVersionUID = 1L;
 
 	public ExcecaoPersonalizada(String message) {
